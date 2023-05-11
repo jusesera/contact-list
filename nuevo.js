70438340c5d7c7cf59bf21bf31117bd5164e593b
Juan Sebastian Sepulveda <jusesera2003@gmail.com>
2023-05-11 16:44:31 -0500

    se actualizo lista de contactos

--- nuevo.js
+++ nuevo.js
@@ -29,10 +29,23 @@ function eliminarContacto(nombre) {
     console.log("Contacto no encontrado:", nombre);
   }
 }
-agregarContacto("Eteban","Godoy Sanchez","321343456", "bogota","carrera 78");
+function actualizarContacto(nombre, nuevoTelefono) {
+  const indice = contactos.findIndex(contacto => contacto.nombre === nombre);
+  if (indice !== -1) {
+    contactos[indice].telefono = nuevoTelefono;
+    console.log("Contacto actualizado:", nombre, nuevoTelefono);
+  } else {
+    console.log("Contacto no encontrado:", nombre);
+  }
+}
+
+agregarContacto("Esteban","Godoy Sanchez","321343456", "bogota","carrera 78");
 agregarContacto("Pedro","Gaviria","1234567890","Santa Marta","carrera 25");
 agregarContacto("Thomas","Mendez Cruz","13456734","Cartagena","carrera 18");
 agregarContacto("Pepito","Romero","146890573","Leticia","carrera 32");
+
+actualizarContacto("Esteban", "34657564765245");
+actualizarContacto("Sandra Patricia", "6565656565656565");
 
 eliminarContacto("Pepito");
 eliminarContacto("Carlos");
